aa098dd6e6993351dada69193a49719cc0a64de1
vmarcell96 <vajdam96@gmail.com>
2022-06-17 19:35:03 +0200

    potion entity added

---
+++ Models/Entities/Potion.cs
@@ -0,0 +1,22 @@
+﻿using HogwartsPotions.Models.Enums;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HogwartsPotions.Models.Entities
+{
+    public class Potion
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long ID { get; set; }
+
+        public string Name { get; set; }
+
+        public Student Brewer { get; set; }
+
+        public ICollection<Ingredient> Ingredients { get; set; }
+
+        public BrewingStatus Status { get; set; } = BrewingStatus.Brew;
+
+        public Recipe Recipe { get; set; }
+    }
+}
