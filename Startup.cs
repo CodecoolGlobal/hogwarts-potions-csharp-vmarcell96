c2d1ae9885ce4abfc1bd2b9a7f7eb2278cf7c44e
vmarcell96 <vajdam96@gmail.com>
2022-07-24 23:57:23 +0200

    added cors policy

--- Startup.cs
+++ Startup.cs
@@ -13,16 +13,31 @@ namespace HogwartsPotions
 {
     public class Startup
     {
+        
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
+        string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                    policy =>
+                    {
+                        policy.WithOrigins("http://localhost:3000",
+                            "https://icy-mushroom-0411fdf0f.1.azurestaticapps.net")
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();
+                    });
+            });
             services.AddDbContext<HogwartsContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
@@ -57,6 +72,8 @@ namespace HogwartsPotions
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseCors(MyAllowSpecificOrigins);
 
             app.UseAuthorization();
 
